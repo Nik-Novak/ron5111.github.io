1c939ba06963c03d859887b95cb0c1b9083099b6
Number1Boss <N.Novak360@gmail.com>
2017-09-22 10:35:03 -0400

    Improved logo and Order Now

--- js/custom.js
+++ js/custom.js
@@ -14,10 +14,12 @@
 			$(".navbar-fixed-top").addClass("top-nav-collapse");
             $(".navbar-brand svg").attr("visibility","visible");
             $(".socialfloat").css("visibility","visible");
+            $(".orderfloat").css("visibility","visible");
 		} else {
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
             $(".navbar-brand svg").attr("visibility","hidden");
             $(".socialfloat").css("visibility","hidden");
+            $(".orderfloat").css("visibility","hidden");
 		}
 	});
 
