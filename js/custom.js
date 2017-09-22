32b2558256713ac2eda63ec3a23dddedccc94cfb
Number1Boss <N.Novak360@gmail.com>
2017-09-22 12:02:49 -0400

    Bunch of small fixes

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,7 @@
 
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
-		$('.navbar-nav li a').bind('click', function(event) {
+		$('li a').bind('click', function(event) {
 			var $anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $($anchor.attr('href')).offset().top
